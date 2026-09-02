d33bb565da84ab7606d7c60b2a88b678094c6ff3
nickfff-dev <59580486+nickfff-dev@users.noreply.github.com>
2026-09-02 21:57:55 +0300

    Add deploymentId to Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,5 @@
 import type { NextConfig } from "next"
 
-const nextConfig: NextConfig = {}
+const nextConfig: NextConfig = { deploymentId: process.env.NEXT_DEPLOYMENT_ID };
 
 export default nextConfig
